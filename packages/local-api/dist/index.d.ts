9d7dcf6137e90e2ac7127b3006109a09aafa2cfd
Suchat Vattanasukkamon <suchat.vattanasukkamon@agoda.com>
2021-05-03 18:04:54 +0700

    implement serve command in cli

---
+++ packages/local-api/dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const serve: (filename: string, port: number, dir: string) => void;
---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,5 @@
+export const serve = (filename: string, port: number, dir: string) => {
+  console.log("serving traffic on port", port);
+  console.log("saving/fetching cells from", filename);
+  console.log("that file is in dir", dir);
+};
