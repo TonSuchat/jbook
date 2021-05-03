4d37c44c0da6ed05d4a83491d3742a93194fb385
Suchat Vattanasukkamon <suchat.vattanasukkamon@agoda.com>
2021-05-03 18:39:50 +0700

    -

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,21 @@
-export const serve = (filename: string, port: number, dir: string) => {
-  console.log("serving traffic on port", port);
-  console.log("saving/fetching cells from", filename);
-  console.log("that file is in dir", dir);
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+export const serve = (
+  filename: string,
+  port: number,
+  dir: string
+): Promise<void> => {
+  const app = express();
+  app.use(express.static("../../local-client/build"));
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: "http://localhost:3000",
+  //     ws: true,
+  //     logLevel: "silent",
+  //   })
+  // );
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
